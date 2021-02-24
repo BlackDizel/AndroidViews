30645aaf5fe63c6534ec12f2ae4b46c2f7f90aed
Alexandr <blakdizel@gmail.com>
2021-02-24 21:33:21 +0300

    webview redirects

--- app/src/main/java/io/solvery/lessonviews/MainActivity.java
+++ app/src/main/java/io/solvery/lessonviews/MainActivity.java
@@ -1,34 +1,36 @@
 package io.solvery.lessonviews;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-
-    //build.gradle
-    //constraintlayout
-    //recycleViews
-    //методы жизненного цикла
-    //фрагменты
-    //рантайм першмишен
-    //сервисы
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.tvName);
-        tv.setText("");
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
+        WebView wvLogin = findViewById(R.id.wvLogin);
+        wvLogin.setWebViewClient(client);
+        wvLogin.loadUrl("https://example.com");
+        // if you need js enabled wvLogin.getSettings().setJavaScriptEnabled(true);
     }
+
+    WebViewClient client = new WebViewClient() {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.contains("test.com")) { //check redirect to test.com
+                Uri uri = Uri.parse(url);
+                String token = uri.getQueryParameter("token"); //todo save token
+                //todo close webview
+                return true;
+            }
+            return false;
+        }
+    };
 }
